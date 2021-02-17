497fe706359221fb404c2575eb507b65bd0adfb3
lionel8 <951793596@qq.com>
2021-02-17 20:03:38 +0800

    update

--- SideBar/index.vue
+++ SideBar/index.vue
@@ -1,5 +1,8 @@
 <template>
   <el-scrollbar wrap-class="scrollbar-wrapper">
+    <div class="side-bar-logo">
+      <slot name="sidelogo"></slot>
+    </div>
     <el-menu :show-timeout="200" :default-active="$route.path" :collapse="isCollapse" mode="vertical" background-color="#304156" text-color="#bfcbd9" active-text-color="#409EFF">
       <sidebar-item v-for="route in permission_routers" :key="`${route.path}_${Math.random() * 10000}`" :item="route" :base-path="route.path" />
     </el-menu>
--- TopBar/index.vue
+++ TopBar/index.vue
@@ -1,19 +1,17 @@
 <template>
-  <!-- <el-scrollbar wrap-class="scrollbar-wrapper"> -->
-    <div class="top-bar-box">
-      <div class="top-bar-logo">
-        <slot name="logo"></slot>
-      </div>
-      <div class="top-bar-menu">
-        <el-menu :show-timeout="200" :default-active="$route.path" mode="horizontal" menu-trigger="hover" background-color="#4caf50" text-color="#fff" active-text-color="#409EFF">
-          <topbar-item v-for="route in permission_routers" :key="`${route.path}_${Math.random() * 10000}`" :item="route" :base-path="route.path" style="max-width:120px;float:left;display:block;"/>
-        </el-menu>
-      </div>
-      <div class="top-bar-info">
-        <slot name="info"></slot>
-      </div>
+  <div class="top-bar-box">
+    <div class="top-bar-logo">
+      <slot name="logo"></slot>
     </div>
-  <!-- </el-scrollbar> -->
+    <div class="top-bar-menu">
+      <el-menu :show-timeout="200" :default-active="$route.path" mode="horizontal" menu-trigger="hover" :background-color="bgColor" text-color="#fff" active-text-color="#409EFF">
+        <topbar-item v-for="route in permission_routers" :key="`${route.path}_${Math.random() * 10000}`" :item="route" :base-path="route.path" style="max-width:120px;float:left;display:block;"/>
+      </el-menu>
+    </div>
+    <div class="top-bar-info">
+      <slot name="info"></slot>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -22,6 +20,12 @@ import TopbarItem from './TopbarItem'
 export default {
   name: 'TopBar',
   components: { TopbarItem },
+  props: {
+    bgColor: {
+      type: String,
+      default: '#4caf50'
+    }
+  },
   computed: {
     ...mapGetters([
       'permission_routers',
