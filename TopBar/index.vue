eff6be91ac6ee9b86a847cb0641317925f6fa044
lionel8 <951793596@qq.com>
2021-02-17 13:24:47 +0800

    update

--- TopBar/index.vue
+++ TopBar/index.vue
@@ -4,9 +4,11 @@
       <div class="top-bar-logo">
         <slot name="logo"></slot>
       </div>
-      <el-menu :show-timeout="200" :default-active="$route.path" mode="horizontal" menu-trigger="hover" background-color="#4caf50" text-color="#fff" active-text-color="#409EFF">
-        <topbar-item v-for="route in permission_routers" :key="`${route.path}_${Math.random() * 10000}`" :item="route" :base-path="route.path" style="max-width:120px;float:left;display:block;"/>
-      </el-menu>
+      <div class="top-bar-menu">
+        <el-menu :show-timeout="200" :default-active="$route.path" mode="horizontal" menu-trigger="hover" background-color="#4caf50" text-color="#fff" active-text-color="#409EFF">
+          <topbar-item v-for="route in permission_routers" :key="`${route.path}_${Math.random() * 10000}`" :item="route" :base-path="route.path" style="max-width:120px;float:left;display:block;"/>
+        </el-menu>
+      </div>
       <div class="top-bar-info">
         <slot name="info"></slot>
       </div>
